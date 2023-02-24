e5873f9f0019fe5c2bfaccc1704ccb1bbbc33a78
Danilo Rocha <danilosilvasrocha@gmail.com>
2023-02-24 18:26:25 -0300

    Add: Products page

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,12 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 import Head from "next/head";
 import styles from './styles.module.scss'
 import { canSSRAuth } from "@/utils/canSSRAuth";
 import { Header } from "@/components/Header";
+import { FiRefreshCcw } from "react-icons/fi";
+import { setupAPIClient } from "@/services/api";
+import Modal from 'react-modal';
+import { ModalOrder } from "@/components/ModalOrder";
+
+interface OrderProps {
+  id: string,
+  table: string | number,
+  status: boolean,
+  drafts: boolean,
+  name: string | null
+}
+interface HomeProps {
+  orders: OrderProps[]
+}
+
+export interface OrderItemProps {
+  id: string
+  amount: number
+  order: Order
+  product: Product
+}
+
+export interface Order {
+  id: string
+  table: string | number
+  name: string | null
+  status: boolean
+  draft: boolean
+}
+
+export interface Product {
+  id: string
+  name: string
+  price: string
+  description: string
+}
 
 
-const Dashboard = () => {
+const Dashboard = ({ orders }: HomeProps) => {
+  const [orderList, setOrderList] = useState(orders || [])
 
+  const [modalItem, setModalItem] = useState<OrderItemProps[]>()
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  function handleCloseModal() {
+    setModalIsOpen(false)
+  }
+
+  async function handlOpenModalView(id: string) {
+    const apiClient = setupAPIClient()
+
+    const response = await apiClient.get('/order/detail', {
+      params: {
+        order_id: id
+      }
+    })
+
+    setModalItem(response.data)
+    setModalIsOpen(true)
+  }
+
+  Modal.setAppElement('#__next');
   return (
     <>
       <Head>
@@ -15,9 +74,33 @@ const Dashboard = () => {
 
       <Header />
 
-      <main className={styles.containerCenter}>
-        <h1>Bem vindo ao Painel</h1>
+      <main className={styles.container}>
+
+        <div className={styles.containerHeader}>
+          <h1>Últimos pedidos</h1>
+          <button>
+            <FiRefreshCcw color="#3fffa3" size={25} />
+          </button>
+        </div>
+
+        <article className={styles.listOrders}>
+          {orderList.map(item => (
+            <section key={item.id} className={styles.orderItem}>
+              <button onClick={() => handlOpenModalView(item.id)}>
+                <span>Mesa {item.table}</span>
+              </button>
+            </section>
+          ))}
+        </article>
       </main >
+
+      {modalIsOpen && (
+        <ModalOrder
+          isOpen={modalIsOpen}
+          onRequestClose={handleCloseModal}
+          order={modalItem}
+        />
+      )}
     </>
   )
 }
@@ -25,7 +108,13 @@ const Dashboard = () => {
 export default Dashboard;
 
 export const getServerSideProps = canSSRAuth(async (ctx) => {
+  const apiClient = setupAPIClient(ctx)
+
+  const response = await apiClient.get('/orders').then((response) => { return response }).catch((err) => { return err })
+
   return {
-    props: {}
+    props: {
+      orders: response.data
+    }
   }
 })
--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -1,132 +1,183 @@
-import React, { FormEvent, useState, ChangeEvent } from "react";
-import Head from "next/head";
-import styles from './styles.module.scss'
-import { canSSRAuth } from "@/utils/canSSRAuth";
-import { Header } from "@/components/Header";
-import { setupAPIClient } from "@/services/api";
-import { toast } from 'react-toastify';
+import { useState, ChangeEvent, FormEvent } from 'react'
+import Head from 'next/head';
+import styles from './styles.module.scss';
+import { Header } from '../../components/Header'
+
+import { canSSRAuth } from '../../utils/canSSRAuth'
+
 import { FiUpload } from 'react-icons/fi'
 
-interface ItemProps {
-  id: string,
-  name: string
+import { setupAPIClient } from '../../services/api'
+
+import { toast } from 'react-toastify'
+
+type ItemProps = {
+  id: string;
+  name: string;
 }
 
 interface CategoryProps {
-  categoryList: ItemProps[]
+  categoryList: ItemProps[];
 }
 
-const Product = ({ categoryList }: CategoryProps) => {
-  const [name, setName] = useState("")
-  const [avatarUrl, setAvatarUrl] = useState("")
-  const [imageAvatar, setImageAvatar] = useState(null)
-  const [categories, setCategories] = useState(categoryList)
+export default function Product({ categoryList }: CategoryProps) {
+
+  const [name, setName] = useState('');
+  const [price, setPrice] = useState('');
+  const [description, setDescription] = useState('');
+
+  const [avatarUrl, setAvatarUrl] = useState('');
+  const [imageAvatar, setImageAvatar] = useState(null);
+
+  const [categories, setCategories] = useState(categoryList || [])
   const [categorySelected, setCategorySelected] = useState(0)
 
-  function handleFile(event: ChangeEvent<HTMLInputElement>) {
-    if (!event.target.files) {
-      return
+
+  function handleFile(e: ChangeEvent<HTMLInputElement>) {
+
+    if (!e.target.files) {
+      return;
     }
 
-    const image = event.target.files[0]
+    const image = e.target.files[0];
 
     if (!image) {
-      return
+      return;
     }
 
     if (image.type === 'image/jpeg' || image.type === 'image/png') {
+
       setImageAvatar(image);
-      setAvatarUrl(URL.createObjectURL(event.target.files[0]))
+      setAvatarUrl(URL.createObjectURL(e.target.files[0]))
+
     }
+
+  }
+
+  function handleChangeCategory(event) {
+
+
+    setCategorySelected(event.target.value)
+
   }
 
   async function handleRegister(event: FormEvent) {
     event.preventDefault();
 
-    if (name === "") {
-      return
+    try {
+      const data = new FormData();
+
+      if (name === '' || price === '' || description === '' || imageAvatar === null) {
+        toast.error("Preencha todos os campos!");
+        return;
+      }
+
+      data.append('name', name);
+      data.append('price', price);
+      data.append('description', description);
+      data.append('category_id', categories[categorySelected].id);
+      data.append('file', imageAvatar);
+
+      console.log(data);
+
+
+      const apiClient = setupAPIClient();
+
+      await apiClient.post('/product', data);
+
+      toast.success('Cadastrado com sucesso!')
+
+    } catch (err) {
+      console.log(err);
+      toast.error("Ops erro ao cadastrar!")
     }
 
-    const apiClient = setupAPIClient();
+    setName('');
+    setPrice('');
+    setDescription('')
+    setImageAvatar(null);
+    setAvatarUrl('');
 
-    await apiClient.post('/product', {
-      name,
-    }).then((result) => { toast.success("Categoria cadastrada com sucesso!") }).catch((error) => {
-      if (error.response.status === 400) {
-        console.log(error.response.data);
-
-        toast.error(`Ocorreu um erro na tentativa de cadastro: "${error.response.data.error}"`)
-        return false
-      }
-    })
-
-    setName("")
-  }
-
-  function handleChangeCategory(event) {
-    setCategorySelected(event.target.value)
   }
 
   return (
     <>
       <Head>
-        <title>Novo produto - Sistema Pizzaria</title>
+        <title>Novo produto - Sujeito Pizzaria</title>
       </Head>
+      <div>
+        <Header />
 
-      <Header />
+        <main className={styles.container}>
+          <h1>Novo produto</h1>
 
-      <main className={styles.container}>
-        <h1>Novo produto</h1>
+          <form className={styles.form} onSubmit={handleRegister}>
 
-        <form className={styles.form} onSubmit={handleRegister}>
+            <label className={styles.labelAvatar}>
+              <span>
+                <FiUpload size={30} color="#FFF" />
+              </span>
 
-          <label className={styles.labelAvatar}>
-            <span>
-              <FiUpload color="#fff" size={30} />
-            </span>
+              <input type="file" accept="image/png, image/jpeg" onChange={handleFile} />
 
-            <input type="file" accept="image/png, image/jpeg" onChange={handleFile} />
+              {avatarUrl && (
+                <img
+                  className={styles.preview}
+                  src={avatarUrl}
+                  alt="Foto do produto"
+                  width={250}
+                  height={250}
+                />
+              )}
+
+            </label>
 
 
-            {avatarUrl && (
-              <img className={styles.preview} src={avatarUrl} alt="Foto do produto" width={250} height={250} />
-            )}
-
-
-          </label>
-
-          {categories.length > 0 ? (
-            <select value={categorySelected} onChange={handleChangeCategory}>
-              {categories.map((item, index) => (
-                <option key={item.id} value={index}>
-                  {item.name}
-                </option>
-              ))}
+            <select value={categorySelected} onChange={handleChangeCategory} >
+              {categories.map((item, index) => {
+                return (
+                  <option key={item.id} value={index}>
+                    {item.name}
+                  </option>
+                )
+              })}
             </select>
-          ) : (
-            <select>
-              <option>Cadastre uma categoria</option>
-            </select>
-          )}
 
-          <input className={styles.input} type="text" placeholder="Digite o nome do produto" />
+            <input
+              type="text"
+              placeholder="Digite o nome do produto"
+              className={styles.input}
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
 
-          <input className={styles.input} type="text" placeholder="Preço do produto" />
+            <input
+              type="text"
+              placeholder="Preço do produto"
+              className={styles.input}
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
+            />
 
-          <textarea className={styles.input} placeholder="Descreva seu produto..." />
+            <textarea
+              placeholder="Descreva seu produto..."
+              className={styles.input}
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
 
-          <button type="submit" >
-            Cadastrar
-          </button>
+            <button className={styles.buttonAdd} type="submit">
+              Cadastrar
+            </button>
 
-        </form>
-      </main>
+          </form>
 
+        </main>
+
+      </div>
     </>
   )
 }
-
-export default Product;
 
 export const getServerSideProps = canSSRAuth(async (ctx) => {
 
